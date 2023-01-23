b2fe52d6cd1eacbaa08ba24ea1580614f938f730
Er. Roshan Kale <54720287+RoshanGK-25@users.noreply.github.com>
2023-01-23 11:44:41 +0530

    NEW UPDATE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employee-CRUD-web-api';
+  constructor(private _toastr : ToastrService,){
+
+  }
+  ngOnInit(){
+    this._toastr.success("Success", "User Master");
+    this._toastr.info("Info Success", "User Master");
+    this._toastr.warning("warning", "User Master");
+    this._toastr.error("Error", "User Master");
+  }
 }
