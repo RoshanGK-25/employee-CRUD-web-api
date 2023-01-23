ff3158601c8e38ab1bdc3042e99d9fc45196166c
Er. Roshan Kale <54720287+RoshanGK-25@users.noreply.github.com>
2023-01-23 17:32:53 +0530

    New Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -6,12 +7,36 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit , AfterViewInit{
   title = 'employee-CRUD-web-api';
-  constructor(){
+
+  educationOptions = [
+    '10th pass',
+    'Diploma',
+    'Graduate',
+    'Post Graduate',
+    'Phd'
+  ];
+  employeeForm: any;
+
+  constructor( private fb : FormBuilder ){
 
   }
-  ngOnInit(){
-
+  ngOnInit():void{
+    this.employeeForm = this.fb.group({
+      firstName: this.fb.control('firstName'),
+      lastName: this.fb.control('lastName'),
+      email: this.fb.control('email'),
+      phone: this.fb.control('phone'),
+      birthdate: this.fb.control('birthday'),
+      gender: this.fb.control('gender'),
+      education: this.fb.control('education'),
+      company: this.fb.control('company'),
+      jobexperience: this.fb.control('jobexperience'),
+      salary: this.fb.control('salary'),
+    });
+  }
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HeaderComponent } from './header/header.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
