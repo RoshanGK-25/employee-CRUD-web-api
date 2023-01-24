4a3cbc97dc1d8cfd8b06379b93a893f90bb7b055
Er. Roshan Kale <54720287+RoshanGK-25@users.noreply.github.com>
2023-01-24 17:56:13 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { emp } from './Models/emp.model';
 import { EmployeeService } from './serices/employee.service';
 
@@ -12,7 +12,7 @@ import { EmployeeService } from './serices/employee.service';
 })
 export class AppComponent implements OnInit , AfterViewInit{
   title = 'employee-CRUD-web-api';
-
+  // @ViewChild('fileInput') fileInput : any;
   educationOptions = [
     '10th pass',
     'Diploma',
@@ -42,8 +42,66 @@ export class AppComponent implements OnInit , AfterViewInit{
       jobExperience: this.fb.control(''),
       salary: this.fb.control(''),
     });
+
+    this.emp.getEmployee().subscribe(res =>{
+      console.log(res);
+    });
+    // this.emp.postEmployee().
   }
   ngAfterViewInit(): void {
     throw new Error('Method not implemented.');
+  }
+
+  addEmployee(){
+    let employee : emp = {
+      firstName: this.firstName.value,
+      lastName: this.lastName.value,
+      birthdate: this.birthdate.value,
+      gender: this.gender.value,
+      education: this.educationOptions[parseInt(this.education.value)],
+      company: this.company.value,
+      jobExperience: this.jobExperience.value,
+      salary: this.salary.value
+    }
+    this.emp.postEmployee(employee).subscribe(res => {
+      this.employees.unshift(res);
+      this.clearForm();
+    })
+  }
+
+  clearForm(){
+    this.firstName.setValue(''); 
+    this.lastName.setValue(''); 
+    this.birthdate.setValue(''); 
+    this.gender.setValue(''); 
+    this.education.setValue(''); 
+    this.company.setValue(''); 
+    this.jobExperience.setValue(''); 
+    this.salary.setValue(''); 
+  }
+
+  public get firstName():FormControl{
+    return this.employeeForm.get('firstName') as FormControl;
+  }
+  public get lastName():FormControl{
+    return this.employeeForm.get('lastName') as FormControl;
+  }
+  public get birthdate():FormControl{
+    return this.employeeForm.get('birthdate') as FormControl;
+  }
+  public get gender():FormControl{
+    return this.employeeForm.get('gender') as FormControl;
+  }
+  public get education():FormControl{
+    return this.employeeForm.get('education') as FormControl;
+  }
+  public get company():FormControl{
+    return this.employeeForm.get('company') as FormControl;
+  }
+  public get jobExperience():FormControl{
+    return this.employeeForm.get('jobExperience') as FormControl;
+  }
+  public get salary():FormControl{
+    return this.employeeForm.get('salary') as FormControl;
   }
 }
--- src/app/serices/employee.service.ts
+++ src/app/serices/employee.service.ts
@@ -6,8 +6,8 @@ import { emp } from '../Models/emp.model';
   providedIn: 'root'
 })
 export class EmployeeService {
-  baseUrl = 'https://my.api.mockaroo.com/emp.json';
-  db = 'emp.json';
+  baseUrl = 'https://my.api.mockaroo.com/';
+  db = 'employee.json';
   key = '?key=2e43f6d0';
   constructor(private http : HttpClient) { }
 
@@ -18,7 +18,7 @@ export class EmployeeService {
     return this.http.post<emp>(this.baseUrl + this.db + this.key + '&_method=POST', employee);
   }
   deleteEmployee(id : string){
-    return this.http.delete<emp>(this.baseUrl + this.db + this.key + '&_method=DELETE' + id);
+    return this.http.delete<emp>(this.baseUrl + this.db + this.key + '&_method=DELETE/' + id);
   }
   putEmployee(id : string, employee : emp){
     return this.http.put<emp>(this.baseUrl + this.db + this.key + '&_method=PUT' +id, employee);
